063aa221b859e2a549d203a8a810ae25bb49e484
sundar-2022 <m.n.sundar.2022@gmail.com>
2023-08-02 13:44:09 +0530

    alignment correction done

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,19 @@
-// Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css'; // Import the updated Home.css file
+import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home-container"> {/* Use a specific class for the Home page container */}
-      <h1>Welcome to the Home Page</h1>
-      <div className="home-image-container"> {/* Use a specific class for the Home page image container */}
+    <div className="home-container">
+      <h1 className="welcome-text">Welcome to the Home Page</h1>
+      <div className="home-image-container">
         <Link to="/chat">
           <img src="/Homepage.png" alt="Chat PDF" />
         </Link>
+        <div className="image-text-container">
+          <p className="image-text">PDF TO TEXT CONVERSION (Q/A)</p>
+          <p className="image-text">There is an easy way to edit PDF text: convert your PDF documents to text with the help of this tool</p>
+        </div>
       </div>
     </div>
   );
