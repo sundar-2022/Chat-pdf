4f0a26395387fddf6019604dea648d5b564c46d3
sundar-2022 <m.n.sundar.2022@gmail.com>
2023-08-02 11:36:22 +0530

    the single page pdf view (allignment needs)

--- src/ChatLayout.js
+++ src/ChatLayout.js
@@ -73,3 +73,4 @@ const ChatLayout = () => {
 };
 
 export default ChatLayout;
+
--- src/ChatSection.jsx
+++ src/ChatSection.jsx
@@ -1,8 +1,33 @@
 import React, { useState } from "react";
 import "./ChatSection.css";
 
+
+
 const ChatSection = () => {
-  const [messages, setMessages] = useState([]);
+        const [messages, setMessages] = useState([
+        {
+          author: "bot",
+          type: "text",
+          data: { text: "Welcome to our website! How can I assist you today?" },
+        },
+      ]);
+
+      
+const handleResetChat = () => {
+  setMessages([
+    {
+      author: "bot",
+      type: "text",
+      data: { text: "Welcome to our website! How can I assist you today?" },
+    },
+  ]);
+};
+
+// Function to delete the chat
+const handleDeleteChat = () => {
+  setMessages([]);
+};
+
   const [userInput, setUserInput] = useState("");
 
   const handleUserMessage = () => {
@@ -28,8 +53,16 @@ const ChatSection = () => {
   };
 
   return (
+    
     <div className="chat-section">
-      <div className="chat-window">
+    <div className="chat-header">
+      <h2>Chat Box</h2>
+      <div className="chat-actions">
+          <button onClick={handleResetChat}>Reset</button>
+          <button onClick={handleDeleteChat}>Delete</button>
+        </div>
+    </div>
+      {/* <div className="chat-window"> */}
         {messages.map((message, index) => (
           <div
             key={index}
@@ -37,10 +70,10 @@ const ChatSection = () => {
               message.author === "user" ? "user-message" : "bot-message"
             }`}
           >
-            {message.data.text}
+              <div className="msg-content" style={{width:"200px"}}>{message.data.text}</div>
           </div>
         ))}
-      </div>
+      {/* </div> */}
       <div className="user-input">
         <input
           type="text"
@@ -53,24 +86,27 @@ const ChatSection = () => {
             }
           }}
         />
-      </div>
-      <button className="send-button" onClick={handleUserMessage}>
+
+<button className="send-button" onClick={handleUserMessage}>
        
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="arrow-icon"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M14 5l7 7m0 0l-7 7m7-7H3"
-            />
-          </svg>
-      </button>
+       <svg
+           xmlns="http://www.w3.org/2000/svg"
+           fill="none"
+           viewBox="0 0 24 24"
+           stroke="currentColor"
+           className="arrow-icon"
+         >
+           <path
+             strokeLinecap="round"
+             strokeLinejoin="round"
+             strokeWidth={5}
+             d="M14 5l7 7m0 0l-7 7m7-7H3"
+           />
+         </svg>
+     </button>
+     
+      </div>
+      
     </div>
   );
 };
