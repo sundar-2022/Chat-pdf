1314659ebe37e1585f2cdfd2becf1ce40ee520ec
sundar-2022 <m.n.sundar.2022@gmail.com>
2023-08-09 16:44:31 +0530

    modification of each chat section

--- src/ChatLayout.js
+++ src/ChatLayout.js
@@ -1,12 +1,11 @@
-// ChatLayout.js
 import React, { useRef, useState } from "react";
 import "./ChatLayout.css";
 import ChatSection from "./ChatSection";
 
 const ChatLayout = () => {
   const fileInputRef = useRef(null);
-  const [uploadedPdfs, setUploadedPdfs] = useState([]);
-  const [uploadedPdf, setUploadedPdf] = useState(null);
+  const [pdfDataList, setPdfDataList] = useState([]);
+  const [selectedPdfId, setSelectedPdfId] = useState(null);
 
   const handleFileUpload = () => {
     fileInputRef.current.click();
@@ -14,24 +13,26 @@ const ChatLayout = () => {
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    // Handle the uploaded file (e.g., display PDF content, etc.)
-    console.log("Uploaded file:", file);
-
-    // Check if the file is a PDF
     if (file.type === "application/pdf") {
       const pdfUrl = URL.createObjectURL(file);
-      const pdfName = file.name; // Extract the client-provided PDF name
-      setUploadedPdfs((prevPdfs) => [{ url: pdfUrl, name: pdfName }, ...prevPdfs]);
-      setUploadedPdf(pdfUrl); // Display the latest uploaded PDF
+      const pdfName = file.name;
+      const pdfId = Date.now().toString();
+      const newPdf = { id: pdfId, url: pdfUrl, name: pdfName, chat: [] };
+
+      setPdfDataList((prevList) => [...prevList, newPdf]);
+      setSelectedPdfId(pdfId);
     } else {
       console.error("Uploaded file is not a PDF.");
     }
   };
 
+  const handlePdfSelect = (pdfId) => {
+    setSelectedPdfId(pdfId);
+  };
+
   return (
     <div className="container">
       <div className="sidebar">
-        {/* Sidebar Content */}
         <div style={{ height: "64px", marginBottom: "15px", cursor: "pointer" }}>
           <button className="upload-button" onClick={handleFileUpload}>
             Upload PDF
@@ -45,32 +46,66 @@ const ChatLayout = () => {
           onChange={handleFileChange}
         />
         <div className="uploaded-pdfs">
-          {uploadedPdfs.map((pdfData, index) => (
-            <div key={index} className="uploaded-pdf">
-              <button
-                className={`uploaded-pdf-button ${
-                  pdfData.url === uploadedPdf ? "selected-pdf-button" : ""
-                }`}
-                onClick={() => setUploadedPdf(pdfData.url)}
-                title={pdfData.name} // Display the full name on hover
-              >
-                {pdfData.name}
-              </button>
+          {pdfDataList.map((pdfData) => (
+            <div key={pdfData.id} className="uploaded-pdf">
+              <div className="pdf-info">
+                <button
+                  className={`uploaded-pdf-button ${
+                    pdfData.id === selectedPdfId ? "selected-pdf-button" : ""
+                  }`}
+                  onClick={() => handlePdfSelect(pdfData.id)}
+                  title={pdfData.name}
+                >
+                  {pdfData.name}
+                </button>
+                <button
+                  className="delete-pdf-button"
+                  onClick={() => {
+                    const updatedPdfDataList = pdfDataList.filter(
+                      (pdfDataItem) => pdfDataItem.id !== pdfData.id
+                    );
+                    setPdfDataList(updatedPdfDataList);
+                    if (selectedPdfId === pdfData.id) {
+                      setSelectedPdfId(null);
+                    }
+                  }}
+                  title="Delete PDF"
+                >
+                  🗑️
+                </button>
+              </div>
             </div>
           ))}
         </div>
       </div>
       <div className="pdf-viewer">
-        {/* Display the uploaded PDF document here */}
-        {uploadedPdf && <embed src={uploadedPdf} width="100%" height="100%" />}
+        {pdfDataList.map((pdfData) => (
+          pdfData.id === selectedPdfId && (
+            <embed key={pdfData.id} src={pdfData.url} width="100%" height="100%" type="application/pdf" />
+          )
+        ))}
       </div>
       <div className="chat">
-        {/* Chat Messages Content */}
-        <ChatSection />
+        {pdfDataList.map((pdfData) => (
+          pdfData.id === selectedPdfId && (
+            <ChatSection
+              key={pdfData.id}
+              pdfData={pdfData}
+              onChatUpdate={(newChat) => {
+                const updatedPdfDataList = pdfDataList.map((pdfDataItem) => {
+                  if (pdfDataItem.id === selectedPdfId) {
+                    return { ...pdfDataItem, chat: newChat };
+                  }
+                  return pdfDataItem;
+                });
+                setPdfDataList(updatedPdfDataList);
+              }}
+            />
+          )
+        ))}
       </div>
     </div>
   );
 };
 
 export default ChatLayout;
-
--- src/ChatSection.jsx
+++ src/ChatSection.jsx
@@ -1,79 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ChatSection.css";
 
+const ChatSection = ({ pdfData, onChatUpdate }) => {
+  const [messages, setMessages] = useState(pdfData.chat);
 
+  useEffect(() => {
+    setMessages(pdfData.chat);
+  }, [pdfData.chat]);
 
-const ChatSection = () => {
-        const [messages, setMessages] = useState([
-        {
-          author: "bot",
-          type: "text",
-          data: { text: "Welcome to our website! How can I assist you today?" },
-        },
-      ]);
+  const handleResetChat = () => {
+    const initialMessages = [
+      {
+        author: "bot",
+        type: "text",
+        data: { text: "Welcome to our website! How can I assist you today?" },
+      },
+    ];
+    setMessages(initialMessages);
+    onChatUpdate(initialMessages);
+  };
 
-      
-const handleResetChat = () => {
-  setMessages([
-    {
-      author: "bot",
-      type: "text",
-      data: { text: "Welcome to our website! How can I assist you today?" },
-    },
-  ]);
-};
-
-// Function to delete the chat
-const handleDeleteChat = () => {
-  setMessages([]);
-};
+  const handleDeleteChat = () => {
+    setMessages([]);
+    onChatUpdate([]);
+  };
 
   const [userInput, setUserInput] = useState("");
 
   const handleUserMessage = () => {
-    if (userInput.trim() === "") return; // Don't send empty messages
+    if (userInput.trim() === "") return;
 
     const newMessage = {
       author: "user",
       type: "text",
       data: { text: userInput },
     };
-    setMessages((prevMessages) => [...prevMessages, newMessage]);
-    setUserInput(""); // Clear the input field after sending
 
-    // Simulate bot response after a short delay (you can replace this with an actual API call)
+    const updatedMessages = [...messages, newMessage];
+    setMessages(updatedMessages);
+    setUserInput("");
+
     setTimeout(() => {
       const botResponse = {
         author: "bot",
         type: "text",
         data: { text: "Hello again" },
       };
-      setMessages((prevMessages) => [...prevMessages, botResponse]);
+      const updatedMessagesWithBot = [...updatedMessages, botResponse];
+      setMessages(updatedMessagesWithBot);
+
+      onChatUpdate(updatedMessagesWithBot);
     }, 1000);
   };
 
   return (
-    
     <div className="chat-section">
-    <div className="chat-header">
-      <h2>Chat Box</h2>
-      <div className="chat-actions">
+      <div className="chat-header">
+        <h2>Chat Box</h2>
+        <div className="chat-actions">
           <button onClick={handleResetChat}>Reset</button>
           <button onClick={handleDeleteChat}>Delete</button>
         </div>
-    </div>
-      {/* <div className="chat-window"> */}
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`chat-message ${
-              message.author === "user" ? "user-message" : "bot-message"
-            }`}
-          >
-              <div className="msg-content" style={{width:"200px"}}>{message.data.text}</div>
+      </div>
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          className={`chat-message ${
+            message.author === "user" ? "user-message" : "bot-message"
+          }`}
+        >
+          <div className="msg-content" style={{ width: "200px" }}>
+            {message.data.text}
           </div>
-        ))}
-      {/* </div> */}
+        </div>
+      ))}
       <div className="user-input">
         <input
           type="text"
@@ -86,27 +86,23 @@ const handleDeleteChat = () => {
             }
           }}
         />
-
-<button className="send-button" onClick={handleUserMessage}>
-       
-       <svg
-           xmlns="http://www.w3.org/2000/svg"
-           fill="none"
-           viewBox="0 0 24 24"
-           stroke="currentColor"
-           className="arrow-icon"
-         >
-           <path
-             strokeLinecap="round"
-             strokeLinejoin="round"
-             strokeWidth={5}
-             d="M14 5l7 7m0 0l-7 7m7-7H3"
-           />
-         </svg>
-     </button>
-     
+        <button className="send-button" onClick={handleUserMessage}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="arrow-icon"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={5}
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            />
+          </svg>
+        </button>
       </div>
-      
     </div>
   );
 };
